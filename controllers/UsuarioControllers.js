468f68f4a3f2c1af51bd363e436e3f7004e6148d
mariozan <marioal1992@hotmail.com>
2024-06-07 20:14:28 -0500

    CRUD completo

--- controllers/UsuarioControllers.js
+++ controllers/UsuarioControllers.js
@@ -1,5 +1,6 @@
 const UserSchema = require("../models/Usuario") // Accedemos a los datos del modelo
 
+// Permite agrupar atributos y funciones
 class UsuarioController {
 
     async getUsuarios(req, res) {
@@ -19,6 +20,36 @@ class UsuarioController {
         await UserSchema(nuevoUsuario).save();
 
         res.send("Guardado correctamente")
+    }
+
+    async getUsuarioById(req, res){
+        var id = req.params.id
+        var usuario = await UserSchema.findById(id)
+        res.json(usuario)
+    }
+
+    async updateUsuario(req, res){
+
+        var id = req.params.id;
+
+        var updateUser = {
+            nombre: req.body.nombre,
+            apellidos: req.body.apellidos,
+            correo: req.body.correo,
+            password: req.body.password,
+        }
+
+        await UserSchema.findByIdAndUpdate(id, updateUser, { new: true })
+
+        res.json({"status": "success", "message": "Usuario Actualizado correctamente"})
+    }
+
+    async deleteUsuario(req, res){
+        var id = req.params.id
+
+        await UserSchema.deleteOne({_id: id})
+
+        res.json({"status": "success", "message": "Usuario Eliminador correctamente"})
     }
 }
 
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express') // Importando la libreria
 const app = express() // Inicializamos la variable de la libreria
 const UsuarioController = require('../controllers/UsuarioControllers') // Importando el controlador
-const controller = new UsuarioController(); // Creando una instancia
+const controller = new UsuarioController(); // Creando un objeto de Usuarios
 
 // Creamos nuestros servicios web
-app.get('/usuario', controller.getUsuarios)
-app.post('/usuario', controller.createUsuario)
+app.get('/usuario', controller.getUsuarios) // Obtengo todos los usuarios
+app.post('/usuario', controller.createUsuario) // Creo un usuario
+app.get('/usuario/:id', controller.getUsuarioById) // Consulto un usuario
+app.put('/usuario/:id', controller.updateUsuario) // Actualizo un usuario
+app.delete('/usuario/:id', controller.deleteUsuario) // Elimino un usuario
 
 module.exports = app
